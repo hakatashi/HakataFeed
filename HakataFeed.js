fafc7d6e608818c160aa695fc6afff8a7b7dabc2
Koki Takahashi <hakatasiloving@gmail.com>
2015-02-03 05:16:22 +0900

    Bring logger into releases

--- HakataFeed.js
+++ HakataFeed.js
@@ -18,7 +18,7 @@ app.set('port', config.port || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 app.use(express.favicon());
-app.use(express.logger('dev'));
+app.use(express.logger('default'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(app.router);

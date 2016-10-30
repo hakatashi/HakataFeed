368f781f4667e9e03a4d77d8b9ad855f3fd2e785
Koki Takahashi <hakatasiloving@gmail.com>
2016-10-30 21:58:51 +0900

    Support whitecube content for pixiv feed

--- feeds/pixiv.js
+++ feeds/pixiv.js
@@ -1,5 +1,6 @@
 const moment = require('moment-timezone');
 const cheerio = require('cheerio');
+const querystring = require('querystring');
 const config = require('../config.js');
 
 const Feed = require('./feed.js')
@@ -49,15 +50,38 @@ class PixivFeed extends Feed {
 		this.request({
 			method: 'GET',
 			url: this.mode === 'illust'
-			     ? 'http://www.pixiv.net/bookmark_new_illust.php'
-			     : 'http://www.pixiv.net/novel/bookmark_new.php',
+			     ? `https://www.pixiv.net/rpc/whitecube/index.php?${
+			           querystring.encode({
+			               mode: 'latest_follow',
+			               filter: 'all',
+			               limit: 50,
+			               adult_mode: 'include',
+			               last_illust_id: 0,
+			               last_novel_id: 0,
+			               is_first_page: 1,
+			           })
+			       }`
+			     : `https://www.pixiv.net/rpc/whitecube/index.php?${
+			           querystring.encode({
+			               mode: 'latest_follow',
+			               filter: 'novels',
+			               limit: 50,
+			               adult_mode: 'include',
+			               last_illust_id: 0,
+			               last_novel_id: 0,
+			               is_first_page: 1,
+			           })
+			       }`,
 			followRedirect: false,
-		}, (error, response, body) => {
+			json: true,
+		}, (error, response, data) => {
 			if (error) return done(error);
 			if (response.statusCode !== 200) return done(new Error('Status not OK'));
-			if (body.length === 0) return done(new Error('zero-length content'));
+			if (data.error !== false) return done(new Error('Status not OK'));
+			if (!data.body || !data.body.html) return done(new Error('content not found'))
+			if (data.body.html.length === 0) return done(new Error('zero-length content'));
 
-			done(null, body);
+			done(null, data.body.html);
 		});
 	}
 
@@ -69,16 +93,14 @@ class PixivFeed extends Feed {
 		const {$items, title, alternateLink, selfLink} = (() => {
 			if (this.mode === 'illust') {
 				return {
-					$items: $('.image-item'),
-					title: 'Recent Illusts from pixiv Followers',
-					alternateLink: 'http://www.pixiv.net/bookmark_new_illust.php',
+					title: 'Recent Items from pixiv Followers',
+					alternateLink: 'http://www.pixiv.net/whitecube/all/latest/follow',
 					selfLink: 'http://feed.hakatashi.com/pixiv.atom',
 				};
 			} else {
 				return {
-					$items: $('.novel-items').children('li'),
 					title: 'Recent Novels from pixiv Followers',
-					alternateLink: 'http://www.pixiv.net/novel/bookmark_new.php',
+					alternateLink: 'http://www.pixiv.net/whitecube/all/latest/follow/novels',
 					selfLink: 'http://feed.hakatashi.com/pixiv-novels.atom',
 				};
 			}
@@ -86,74 +108,57 @@ class PixivFeed extends Feed {
 
 		const entries = [];
 
-		$items.each((index, item) => {
+		$('.item-container').each((index, item) => {
 			const $item = $(item);
 
-			if (this.mode === 'illust') {
-				const dateParams = $item.find('._thumbnail').attr('src').split('/').map(param => parseInt(param, 10));
+			const dateParams = $item.find('.image > img').data('src').split('/').map(param => parseInt(param, 10));
+			if (dateParams.length === 12) dateParams.unshift(NaN, NaN);
 
-				if (dateParams.length === 12) dateParams.unshift(NaN, NaN);
+			const $caption = $item.find('.caption');
 
-				var info = {
-					illust_id:    parseInt($item.find('.work').attr('href').match(/illust_id=(\d+)/)[1]),
-					user_id:      parseInt($item.find('.user').data('user_id')),
-					//extension:    row[2],
-					title:        $item.find('.title').text(),
-					user_name:    $item.find('.user').data('user_name'),
-					illust_url:   $item.find('._thumbnail').attr('src').replace('150x150', '480x960'),
-					upload_date:  moment.tz(new Date(
-						dateParams[7],
-						dateParams[8] - 1,
-						dateParams[9],
-						dateParams[10],
-						dateParams[11],
-						dateParams[12]
-					), 'Asia/Tokyo'),
-					tags:         $item.find('._thumbnail').data('tags'),
-					//evaluate_cnt: parseInt(row[15]),
-					//evaluate_sum: parseInt(row[16]),
-					//view_cnt:     parseInt(row[17]),
-					caption:      'Caption unavailable',
-					//page_cnt:     parseInt(row[19]),
-					//r18:          Boolean(row[26] === '1'),
-				};
-			} else {
-				var info = {
-					illust_id:    parseInt($item.find('.title').children('a').attr('href').match(/id=(\d+)/)[1]),
-					user_id:      parseInt($item.find('.author').children('a').data('user_id')),
-					//extension:    row[2],
-					title:        $item.find('.title').children('a').text(),
-					user_name:    $item.find('.author').children('a').data('user_name'),
-					illust_url:   $item.find('.cover').attr('src'),
-					upload_date:  moment.tz(new Date(), 'Asia/Tokyo'),
-					tags:         $item.find('.tags > li > a:nth-child(2)').map(function(){return $(this).text()}).toArray().join(' '),
-					//evaluate_cnt: parseInt(row[15]),
-					//evaluate_sum: parseInt(row[16]),
-					//view_cnt:     parseInt(row[17]),
-					caption:      $item.find('.novel-caption').text(),
-					//page_cnt:     parseInt(row[19]),
-					//r18:          Boolean(row[26] === '1'),
-				};
-			}
+			var info = {
+				illust_id:    parseInt($item.find('.title').data('work-id')),
+				user_id:      parseInt($item.find('.user-view-popup').data('user_id')),
+				//extension:    row[2],
+				title:        $item.find('.title').text(),
+				user_name:    $item.find('.user-name').text(),
+				illust_url:   $item.find('.image > img').data('src').replace(/^.+?\/img/, 'http://i1.pixiv.net/c/480x960/img'),
+				upload_date:  moment.tz(new Date(
+					dateParams[7],
+					dateParams[8] - 1,
+					dateParams[9],
+					dateParams[10],
+					dateParams[11],
+					dateParams[12]
+				), 'Asia/Tokyo'),
+				//tags:         $item.find('._thumbnail').data('tags'),
+				//evaluate_cnt: parseInt(row[15]),
+				//evaluate_sum: parseInt(row[16]),
+				//view_cnt:     parseInt(row[17]),
+				caption:      $caption.length > 0 ? $caption.text() : 'Caption Unavailable',
+				//page_cnt:     parseInt(row[19]),
+				//r18:          Boolean(row[26] === '1'),
+				mode:         $item.find('.title').data('activity-work_type'),
+			};
 
 			const user_url = `http://www.pixiv.net/whitecube/user/${info.user_id}`;
 
 			let url, content, category;
 
-			if (this.mode === 'illust') {
+			if (info.mode === 'illust') {
 				url = `http://www.pixiv.net/whitecube/illust/${info.illust_id}`;
 
 				category = $item.find('.work').hasClass('manga') ? 'manga' : 'illust';
 
 				content =
-					`<p>${info.caption}</p><p>タグ: ${info.tags}</p><p><a href="${url}"><img src="${info.illust_url}" /></a></p>`;
+					`<p>${info.caption}</p><p><a href="${url}"><img src="${info.illust_url}" /></a></p>`;
 			} else {
 				url = `http://www.pixiv.net/whitecube/novel/${info.illust_id}`;
 
 				category = 'novel';
 
 				content =
-					`<p>${info.caption}</p><p>タグ: ${info.tags}</p>`;
+					`<p>${info.caption}</p>`;
 			}
 
 			const entry = {
